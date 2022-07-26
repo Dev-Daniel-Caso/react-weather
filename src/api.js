ffe9c78fb853dd1db64596caf72a66ada030aeff
Daniel Caso <71891927+Dev-Daniel-Caso@users.noreply.github.com>
2022-07-26 15:08:40 -0500

    Add component search and autocomplete in input of cities

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3edafef55cmsh4ace2159a4ac150p166bf9jsn364794a744d3',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
